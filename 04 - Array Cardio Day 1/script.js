d8cb6b99c3496c9148e62c35a2f3e5566e4b3c96
Brianna Tran <briannatran45@gmail.com>
2024-02-16 13:33:06 -0800

    finished paris ex

--- 04 - Array Cardio Day 1/script.js
+++ 04 - Array Cardio Day 1/script.js
@@ -56,6 +56,13 @@ console.table(yearsLived);
 
 // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
 // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
+const category = document.querySelector('.mw-category');
+// NodeList, need to turn into arr
+const links = Array.from(category.querySelectorAll('a'));
+
+const de = links
+  .map(link => link.textContent)
+  .filter(name => name.includes('de'));
 
 
 // 7. sort Exercise
