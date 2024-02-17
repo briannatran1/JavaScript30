369015c1f3c41e22bdb110d8ed451d01dbc8acaf
Brianna Tran <briannatran45@gmail.com>
2024-02-17 13:34:47 -0800

    added functionality

--- 05 - Flex Panel Gallery/script.js
+++ 05 - Flex Panel Gallery/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const panels = document.querySelectorAll('.panel'); //NodeList
+
+/** adds class open to elem */
+function toggleOpen() {
+  this.classList.toggle('open');
+}
+
+/** adds class open-active if elem is clicked and adds top and bottom text */
+function toggleActive(evt) {
+  if (evt.propertyName.includes('flex')) {
+    this.classList.toggle('open-active');
+  }
+}
+
+panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
