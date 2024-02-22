17d6cf3552ec88ca7e8a7adca0660bc0cafece2f
Brianna Tran <briannatran45@gmail.com>
2024-02-22 11:48:46 -0800

    added functionality

--- 10 - Hold Shift and Check Checkboxes/script.js
+++ 10 - Hold Shift and Check Checkboxes/script.js
@@ -1,2 +1,26 @@
 'use strict';
 
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+let lastChecked;
+
+/** handles checking the checkboxes */
+function handleCheck(evt) {
+  let inBetween = false;
+  // check if shift key is down
+  // and check if they are checking the box
+  if (evt.shiftKey && this.checked) {
+    checkboxes.forEach(checkbox => {
+      if (checkbox === this || checkbox === lastChecked) {
+        inBetween = !inBetween;
+      }
+
+      if (inBetween) {
+        checkbox.checked = true;
+      }
+    });
+  }
+
+  lastChecked = this;
+}
+
+checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
