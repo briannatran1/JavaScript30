63eccf35375474dddc905367ce1ebe69c2ce1de2
Brianna Tran <briannatran45@gmail.com>
2024-02-21 18:28:43 -0800

    initial files

---
+++ 09 - Dev Tools Domination/script.js
@@ -0,0 +1,34 @@
+'use script';
+
+const dogs = [{ name: 'Snickers', age: 2 }, { name: 'hugo', age: 8 }];
+
+/** makes p green */
+function makeGreen() {
+  const p = document.querySelector('p');
+  p.style.color = '#BADA55';
+  p.style.fontSize = '50px';
+}
+
+// Regular
+
+// Interpolated
+
+// Styled
+
+// warning!
+
+// Error :|
+
+// Info
+
+// Testing
+
+// clearing
+
+// Viewing DOM Elements
+
+// Grouping together
+
+// counting
+
+// timing
