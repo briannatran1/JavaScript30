729da203cbcf6c0d786e9d07d9501df1c193349a
Brianna Tran <briannatran45@gmail.com>
2024-02-25 12:36:51 -0800

    grabbed all elems

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,1 +1,14 @@
 'use strict';
+
+// get our elems
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progessBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelector('[data-skip]');
+const ranges = player.querySelector('.player__slider');
+
+// build our fns
+// hook up event listeners
+
