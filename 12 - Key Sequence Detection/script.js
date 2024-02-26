8f38a68ac02571fcc35b033670399e8134e3aec4
Brianna Tran <briannatran45@gmail.com>
2024-02-26 13:00:47 -0800

    finished

--- 12 - Key Sequence Detection/script.js
+++ 12 - Key Sequence Detection/script.js
@@ -1,2 +1,17 @@
 'use strict';
 
+// store in arr
+// check every single time if keyup matches what you're looking for
+
+const pressed = [];
+const secretCode = 'wesbos';
+
+window.addEventListener('keyup', (evt) => {
+  pressed.push(evt.key);
+  pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+
+  if (pressed.join('').includes(secretCode)) {
+    cornify_add();
+  }
+  console.log(pressed);
+});
