f870533c19a6dad7f66d4ed4810aa5673f0d9fdd
Brianna Tran <briannatran45@gmail.com>
2024-02-27 20:00:35 -0800

    finished

--- 13 - Slide in on Scroll/script.js
+++ 13 - Slide in on Scroll/script.js
@@ -1,5 +1,28 @@
 'use strict';
 
+// select imgs
+const sliderImages = document.querySelectorAll('.slide-in');
+
+/** checks positioning of scroll */
+function checkSlide(evt) {
+  for (let img of sliderImages) {
+    // half way through the image
+    const slideInAt = (window.scrollY + window.innerHeight) - img.height / 2;
+    // bottom of image
+    const imageBottom = img.offsetTop + img.height;
+    const isHalfShown = slideInAt > img.offsetTop;
+    const isNotScrolledPast = window.scrollY < imageBottom;
+
+    if (isHalfShown && isNotScrolledPast) {
+      img.classList.add('active');
+    }
+    else {
+      img.classList.remove('active');
+    }
+  }
+}
+
+/** brings image back on page */
 function debounce(func, wait = 20, immediate = true) {
   var timeout;
   return function () {
@@ -14,3 +37,5 @@ function debounce(func, wait = 20, immediate = true) {
     if (callNow) func.apply(context, args);
   };
 }
+
+window.addEventListener('scroll', debounce(checkSlide));
