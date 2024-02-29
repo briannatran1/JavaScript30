08d19225573d4ed1b962f2c6e4555f57d0b126dd
Brianna Tran <briannatran45@gmail.com>
2024-02-29 13:21:33 -0800

    copies of arrs

--- 14 - JavaScript References VS Copying/script.js
+++ 14 - JavaScript References VS Copying/script.js
@@ -1,15 +1,30 @@
 'use strict';
 
 // start with strings, numbers and booleans
+let age = 100;
+let age2 = age;
+console.log(age, age2);
+age = 200;
+console.log(age, age2);
+
+let name1 = 'Wes';
+let name2 = name1;
+console.log(name1, name2);
+name1 = 'wesley';
+console.log(name1, name2);
 
 // Let's say we have an array
 const players = ['Wes', 'Sarah', 'Ryan', 'Poppy'];
 
 // and we want to make a copy of it.
+const team = players;
+console.log(players, team);
 
 // You might think we can just do something like this:
+team[3] = 'Lux';
 
 // however what happens when we update that array?
+
 
 // now here is the problem!
 
@@ -17,13 +32,17 @@ const players = ['Wes', 'Sarah', 'Ryan', 'Poppy'];
 
 // Why? It's because that is an array reference, not an array copy. They both point to the same array!
 
-// So, how do we fix this? We take a copy instead!
+
+// So, how do we fix this? We take a shallow copy instead!
+const team2 = players.slice();
 
 // one way
 
 // or create a new array and concat the old one in
+const team3 = [].concat(players);
 
 // or use the new ES6 Spread
+const team4 = [...players];
 
 // now when we update it, the original one isn't changed
 
