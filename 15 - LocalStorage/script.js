4a3af748708520d79b82d9456d14dced2e010747
Brianna Tran <briannatran45@gmail.com>
2024-03-05 20:48:01 -0800

    addItem fn

--- 15 - LocalStorage/script.js
+++ 15 - LocalStorage/script.js
@@ -3,3 +3,24 @@
 const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
 const items = [];
+
+/** adds item to list */
+function addItem(evt) {
+  evt.preventDefault();
+  const text = (this.querySelector('[name=item]')).value;
+  const item = {
+    text: text,
+    done: false
+  };
+
+  items.push(item);
+  // this = form element
+  this.reset();
+}
+
+/** adds item to list page */
+function populateList() {
+
+}
+
+addItems.addEventListener('submit', addItem);
