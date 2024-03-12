d39e46d79309b6a8403fdee9fcc0659fcf18ff0a
Brianna Tran <briannatran45@gmail.com>
2024-03-12 13:54:14 -0700

    added offsets to x and y

--- 16 - Mouse Move Shadow/script.js
+++ 16 - Mouse Move Shadow/script.js
@@ -1,2 +1,19 @@
 'use strict';
 
+const hero = document.querySelector('.hero');
+const text = document.querySelector('h1');
+
+/** creates shadow */
+function shadow(evt) {
+  const { offsetWidth: width, offsetHeight: height } = hero;
+  let { offsetX: x, offsetY: y } = evt;
+
+  if (this !== evt.target) { // this is equal to hero since it's what's being listened
+    x = x + evt.target.offsetLeft;
+    y = y + evt.target.offsetTop;
+  }
+
+
+}
+
+hero.addEventListener('mousemove', shadow);
