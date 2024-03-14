807f24cc3c3bae588bce9dd60563f3969e41052f
Brianna Tran <briannatran45@gmail.com>
2024-03-14 13:30:34 -0700

    finished

--- 17 - Sort Without Articles/script.js
+++ 17 - Sort Without Articles/script.js
@@ -4,28 +4,30 @@ const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil',
   'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans',
   'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
 
-// iterate through bands and check if there's an article
-// if there is one, take it out
-
+// removed for regex fn + didn't want to modify og arr
+//
 /** takes out article in word */
-function articleOut(word) {
-  const articles = ["The", "An", "A"];
-  const separateWord = word.split(' ');
+// function articleOut(word) {
+//   const articles = ["The", "An", "A"];
+//   const separateWord = word.split(' ');
 
-  for (let article of articles) {
-    if (article.includes(word[0])) {
-      word = separateWord.splice(1);
-    }
-  }
+//   for (let article of articles) {
+//     if (article.includes(word[0])) {
+//       word = separateWord.splice(1);
+//     }
+//   }
 
-  return word.join(' ');
-}
+//   return word.join(' ');
+// }
 
+/** replaces article in word */
 function strip(band) {
   return band.replace(/^(a |the |an )/i, '').trim();
 }
 
-/** sorts bands in alphabetical order */
-function sortBands(bands) {
-  const sortedBands = bands.sort((a, b) => a - b);
-}
+const sortedBands = bands.sort((a, b) => strip(a) > strip(b) ? 1 : -1);
+
+document.querySelector('#bands').innerHTML =
+  sortedBands
+    .map(band => `<li>${band}</li>`)
+    .join('');
