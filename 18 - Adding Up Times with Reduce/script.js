2a27b224326557542691e56011b038b9964faa68
Brianna Tran <briannatran45@gmail.com>
2024-03-15 13:50:56 -0700

    seconds

--- 18 - Adding Up Times with Reduce/script.js
+++ 18 - Adding Up Times with Reduce/script.js
@@ -1,2 +1,12 @@
 'use strict';
 
+const timeNodes = Array.from(document.querySelectorAll('[data-time]'));
+
+const seconds = timeNodes
+  .map(node => node.dataset.time)
+  .map(timeCode => {
+    const [mins, secs] = timeCode.split(':').map(Number);
+
+    return (mins * 60) + secs;
+  })
+
